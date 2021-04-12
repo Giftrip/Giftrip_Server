4b0b34313518b7ba843ed53745b6f7ad56ea07bf
Sh031224 <1cktmdgh2@gmail.com>
2021-04-12 15:44:10 +0900

    Init: Project Setting

---
+++ src/main/kotlin/com/flash21/giftrip/GiftripApplication.kt
@@ -0,0 +1,11 @@
+package com.flash21.giftrip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GiftripApplication
+
+fun main(args: Array<String>) {
+	runApplication<GiftripApplication>(*args)
+}
---
+++ src/test/kotlin/com/flash21/giftrip/GiftripApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.flash21.giftrip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GiftripApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
