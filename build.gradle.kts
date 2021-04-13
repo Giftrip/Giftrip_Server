a8e9e6a9476a94063a9925f1670b1c04a9185281
Sh031224 <1cktmdgh2@gmail.com>
2021-04-13 20:39:01 +0900

    Add: Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,17 @@ repositories {
 }
 
 dependencies {
+	// 이거 없으면 시작하자마자 꺼짐
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.hibernate:hibernate-validator:6.1.5.Final")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
+	implementation("io.jsonwebtoken:jjwt:0.5.1")
+
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+	implementation("org.springframework.boot:spring-boot-configuration-processor")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
